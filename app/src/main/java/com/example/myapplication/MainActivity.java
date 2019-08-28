9776bf0d71d7d7201e02c52b31339f0ae184b6d7
Arthur <957980613@qq.com>
2019-08-28 22:10:20 +0800

    whole project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initviews();
+    }
+    public void initviews()
+    {
+        TextView tv1=findViewById(R.id.textView1);
+        tv1.setText(getResources().getString(R.string.title));
+        tv1.setTextSize(TypedValue.COMPLEX_UNIT_DIP,getResources().getDimension(R.dimen.title));
+
+        TextView tv2=findViewById(R.id.textView2);
+        tv2.setText(getResources().getString(R.string.news));
+        tv2.setTextSize(TypedValue.COMPLEX_UNIT_DIP,getResources().getDimension(R.dimen.body));
+
+
+        ScrollView sv=findViewById(R.id.id_scrollView);
+        ImageView img=findViewById(R.id.imageView);
+        img.setImageDrawable(getResources().getDrawable(R.drawable.newstest));
+    }
+
+}
