50b1aa795df4d55a8063379f063edc7b0a7f807d
Neptune <1115876559@qq.com>
2019-09-07 00:07:01 +0800

    Share Done

--- app/src/main/java/com/example/panjunchen/ReadActivity.java
+++ app/src/main/java/com/example/panjunchen/ReadActivity.java
@@ -79,19 +79,9 @@ public class ReadActivity extends AppCompatActivity {
                     case R.id.action_share:
                     {
                         OnekeyShare oks = new OnekeyShare();
-                        // title标题，微信、QQ和QQ空间等平台使用
                         oks.setTitle(news.getTitle());
-                        // titleUrl QQ和QQ空间跳转链接
-                        oks.setTitleUrl("http://sharesdk.cn");
-                        // text是分享文本，所有平台都需要这个字段
-                        oks.setText("我是分享文本");
-                        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-                        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
-                        // url在微信、微博，Facebook等平台中使用
-                        oks.setUrl("http://sharesdk.cn");
-                        // comment是我对这条分享的评论，仅在人人网使用
-                        oks.setComment("我是测试评论文本");
-// 启动分享GUI
+                        oks.setText(news.getTitle());
+                        oks.setUrl(news.getPageURL());
                         oks.show(getApplicationContext());
                         break;
                     }
